af707b135fb2788b8ba7259fa14b24675e5b3532
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-07-03 10:39:48 +0200

    Update Rock_paper_Scissors_Lizard_Spock.py

--- Rock_paper_Scissors_Lizard_Spock.py
+++ Rock_paper_Scissors_Lizard_Spock.py
@@ -13,60 +13,67 @@
 
 # Code:
 import random 
-print("==================") 
-print("Hi! Do you know a simple game of Rock Paper Scissors? Let's add a little twist!")
-print("This game is Rock Paper Scissors Lizard Spock! \n To remind you, here are the rules: \n")
-print(" Scissors cut Paper \n Paper covers Rock \n Rock crushes Lizard \n Lizard poisons Spock \n Spock smashes Scissors \n Scissors beat Lizard \n Lizard eats Paper \n Paper disproves Spock \n Spock vaporizes Rock \n Rock breaks Scissors")
-print(" 1) ✊ \n 2) ✋ \n 3) ✌ \n 4) 🦎 \n 5) 🖖")
-computer = int(random.randint(1, 5))
-player = int(input("Pick a number: "))
-if player == 1 and computer == 1:
-  print("You chose: ✊ \n Computer chose: ✊ \n It's a tie!")
-elif player == 1 and computer == 2:
-  print("You chose: ✊ \n Computer chose: ✋ \n Computer wins!")
-elif player == 1 and computer == 3:
-  print("You chose: ✊ \n Computer chose: ✌ \n You win!")
-elif player == 1 and computer == 4:
-  print("You chose: ✊ \n Computer chose: 🦎  \n You win!")
-elif player == 1 and computer == 5:
-  print("You chose: ✊ \n Computer chose: 🖖  \n Spock wins!")
-elif player == 2 and computer == 1:
-  print("You chose: ✋ \n Computer chose: ✊ \n You win!")
-elif player == 2 and computer == 2:
-  print("You chose: ✋ \n Computer chose: ✋ \n It's a tie!")
-elif player == 2 and computer == 3:
-  print("You chose: ✋ \n Computer chose: ✌ \n Computer wins!")
-elif player == 2 and computer == 4:
-  print("You chose: ✋ \n Computer chose: 🦎  \n Computer wins!")
-elif player == 2 and computer == 5: 
-  print("You chose: ✋ \n Computer chose: 🖖  \n You win!")
-elif player == 3 and computer == 1:
-  print("You chose: ✌ \n Computer chose: ✊ \n Computer wins!")
-elif player == 3 and computer == 2: 
-  print("You chose: ✌ \n Computer chose: ✋ \n You win!")
-elif player == 3 and computer == 3: 
-  print("You chose: ✌ \n Computer chose: ✌ \n It's a tie!")
-elif player == 3 and computer == 4:
-  print("You chose: ✌ \n Computer chose: 🦎  \n You win!")
-elif player == 3 and computer == 5: 
-  print("You chose: ✌ \n Computer chose: 🖖  \n Spock wins!")
-elif player == 4 and computer == 1: 
-  print("You chose: 🦎 \n Computer chose: ✊  \n Computer wins!")
-elif player == 4 and computer == 2: 
-  print("You chose: 🦎 \n Computer chose: ✋  \n You win!")
-elif player == 4 and computer == 3:
-  print("You chose: 🦎 \n Computer chose: ✌  \n Computer wins!")
-elif player == 4 and computer == 4:
-  print("You chose: 🦎 \n Computer chose: 🦎 \n It's a tie!")
-elif player == 4 and computer == 5:
-  print("You chose: 🦎 \n Computer chose: 🖖 \n You win!")
-elif player == 5 and computer == 1: 
-  print("You chose: 🖖 \n Computer chose: ✊ \n Spock wins!") 
-elif player == 5 and computer == 2: 
-  print("You chose: 🖖 \n Computer chose: ✋ \n Computer wins!")
-elif player == 5 and computer == 3:
-  print("You chose: 🖖 \n Computer chose: ✌ \n Spock wins!")
-elif player == 5 and computer == 4: 
-  print("You chose: 🖖 \n Computer chose: 🦎 \n Computer wins!")
-elif player == 5 and computer == 5: 
-  print("You chose: 🖖 \n Computer chose: 🖖 \n It's a tie!")
+again = "yes" or "Yes"
+while again == "yes" or "Yes": 
+
+  print("==================") 
+  print("Hi! Do you know a simple game of Rock Paper Scissors? Let's add a little twist!")
+  print("This game is Rock Paper Scissors Lizard Spock! \n To remind you, here are the rules: \n")
+  print(" Scissors cut Paper \n Paper covers Rock \n Rock crushes Lizard \n Lizard poisons Spock \n Spock smashes Scissors \n Scissors beat Lizard \n Lizard eats Paper \n Paper disproves Spock \n Spock vaporizes Rock \n Rock breaks Scissors")
+  print(" 1) ✊ \n 2) ✋ \n 3) ✌ \n 4) 🦎 \n 5) 🖖")
+  computer = int(random.randint(1, 5))
+  player = int(input("Pick a number: "))
+  if player == 1 and computer == 1:
+    print("You chose: ✊ \n Computer chose: ✊ \n It's a tie!")
+  elif player == 1 and computer == 2:
+    print("You chose: ✊ \n Computer chose: ✋ \n Computer wins!")
+  elif player == 1 and computer == 3:
+    print("You chose: ✊ \n Computer chose: ✌ \n You win!")
+  elif player == 1 and computer == 4:
+    print("You chose: ✊ \n Computer chose: 🦎  \n You win!")
+  elif player == 1 and computer == 5:
+    print("You chose: ✊ \n Computer chose: 🖖  \n Spock wins!")
+  elif player == 2 and computer == 1:
+    print("You chose: ✋ \n Computer chose: ✊ \n You win!")
+  elif player == 2 and computer == 2:
+    print("You chose: ✋ \n Computer chose: ✋ \n It's a tie!")
+  elif player == 2 and computer == 3:
+    print("You chose: ✋ \n Computer chose: ✌ \n Computer wins!")
+  elif player == 2 and computer == 4:
+    print("You chose: ✋ \n Computer chose: 🦎  \n Computer wins!")
+  elif player == 2 and computer == 5: 
+    print("You chose: ✋ \n Computer chose: 🖖  \n You win!")
+  elif player == 3 and computer == 1:
+    print("You chose: ✌ \n Computer chose: ✊ \n Computer wins!")
+  elif player == 3 and computer == 2: 
+    print("You chose: ✌ \n Computer chose: ✋ \n You win!")
+  elif player == 3 and computer == 3: 
+    print("You chose: ✌ \n Computer chose: ✌ \n It's a tie!")
+  elif player == 3 and computer == 4:
+    print("You chose: ✌ \n Computer chose: 🦎  \n You win!")
+  elif player == 3 and computer == 5: 
+    print("You chose: ✌ \n Computer chose: 🖖  \n Spock wins!")
+  elif player == 4 and computer == 1: 
+    print("You chose: 🦎 \n Computer chose: ✊  \n Computer wins!")
+  elif player == 4 and computer == 2: 
+    print("You chose: 🦎 \n Computer chose: ✋  \n You win!")
+  elif player == 4 and computer == 3:
+    print("You chose: 🦎 \n Computer chose: ✌  \n Computer wins!")
+  elif player == 4 and computer == 4:
+    print("You chose: 🦎 \n Computer chose: 🦎 \n It's a tie!")
+  elif player == 4 and computer == 5:
+    print("You chose: 🦎 \n Computer chose: 🖖 \n You win!")
+  elif player == 5 and computer == 1: 
+    print("You chose: 🖖 \n Computer chose: ✊ \n Spock wins!") 
+  elif player == 5 and computer == 2: 
+    print("You chose: 🖖 \n Computer chose: ✋ \n Computer wins!")
+  elif player == 5 and computer == 3:
+    print("You chose: 🖖 \n Computer chose: ✌ \n Spock wins!")
+  elif player == 5 and computer == 4: 
+    print("You chose: 🖖 \n Computer chose: 🦎 \n Computer wins!")
+  elif player == 5 and computer == 5: 
+    print("You chose: 🖖 \n Computer chose: 🖖 \n It's a tie!")
+  else:
+    print("Invalid choice, try again")
+  again = input(" Do you want to play again? (yes/no): ")
+
